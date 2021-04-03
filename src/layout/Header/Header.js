9cfa4ccc8a8b8c8522f887679dc932b83d195223
dhimanroyit <dhimanroyict@gmail.com>
2021-04-03 07:23:43 +0600

    add avatar component to header

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import {signInContext} from '../../context/SignInContext';
+import Avatar from '../../components/Avatar/Avatar';
 import './Header.css';
 
 function Header() {
+  const {signInUser} = useContext(signInContext)
   return (
     <header className="header container">
       <h2 className="logo">BookStore</h2>
@@ -12,7 +15,7 @@ function Header() {
         <Link to="/admin" className="nav__item">Admin</Link>
         <Link to="/deals" className="nav__item">Deals</Link>
         <Link to="/checkout" className="nav__item">Checkout</Link>
-        <Link to="/signin" className="nav__item">Login</Link>
+        {signInUser.signIn ? <Avatar img={signInUser.photo} /> : <Link to="/signin" className="nav__item">Login</Link>}
       </nav>
     </header>
   )
