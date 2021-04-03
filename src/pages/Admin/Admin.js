9a971edcb4c9b8e74d071136b64c1a876a84cc16
dhimanroyit <dhimanroyict@gmail.com>
2021-04-03 07:05:50 +0600

    update component ManageBooks to ManageProducts

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -3,22 +3,42 @@ import {Link, useRouteMatch, Route} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faPlus, faEdit, faThLarge} from '@fortawesome/free-solid-svg-icons';
 import AddProduct from '../AddProduct/AddProduct';
-import ManageBooks from '../ManageBooks/ManageBooks';
+import ManageProducts from '../ManageBooks/ManageProducts';
 import './Admin.css';
 
 
 function Admin() {
   const {url, path} = useRouteMatch();
   
-  console.log(url, path);
   return (
     <div className="admin">
       <div className="sidebar">
         <h2 className="sidebar__logo">BookStore</h2>
         <div className="sidebar__nav">
-          <Link to={`${url}/managebooks`} className="sidebar__navItem"> <FontAwesomeIcon className="sidebar__navIcon" icon={faThLarge} />Manage Books</Link>
-          <Link to={`${url}/addProduct`} className="sidebar__navItem"><FontAwesomeIcon className="sidebar__navIcon" icon={faPlus} />Add Book</Link>
-          <Link to={`${url}/editbook`} className="sidebar__navItem"><FontAwesomeIcon className="sidebar__navIcon" icon={faEdit} />Edit Book</Link>
+          <Link 
+            to={`${url}/manageproducts`} 
+            className="sidebar__navItem">
+              <FontAwesomeIcon 
+                className="sidebar__navIcon" 
+                icon={faThLarge} />
+                Manage Books
+          </Link>
+          <Link 
+            to={`${url}/addproduct`} 
+            className="sidebar__navItem">
+              <FontAwesomeIcon 
+                className="sidebar__navIcon" 
+                icon={faPlus} />
+                Add Book
+          </Link>
+          <Link 
+            to={`${url}/editbook`} 
+            className="sidebar__navItem">
+              <FontAwesomeIcon 
+                className="sidebar__navIcon" 
+                icon={faEdit} />
+                Edit Book
+          </Link>
         </div>
       </div>
       <div className="admin__page">
@@ -26,14 +46,14 @@ function Admin() {
           <h2 className="admin__pageTitle">AddBook</h2>
         </header>
         <section className="admin__pageContent">
-          <Route path={`${path}/managebooks`}>
-            <ManageBooks />
+          <Route path={`${path}/manageproducts`}>
+            <ManageProducts />
           </Route>
-          <Route path={`${path}/addProduct`} >
+          <Route path={`${path}/addproduct`} >
             <AddProduct />           
           </Route>
           <Route path={`${path}/editbook`} >
-          <h1>edit book</h1>
+            <h1>edit book</h1>
           </Route>
         </section>
       </div>
